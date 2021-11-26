f1d12187ea2fb0fb0f0f53cb298c226e497e3ac7
Shadee Merhi <shadmerhi@gmail.com>
2021-11-26 01:25:40 -0700

    add getRtpCapabilities and createDevice

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 const app = express();
 import http from 'http';
+import mediasoup from 'mediasoup';
 
 import https from 'httpolyglot';
 import fs from 'fs';
 import path from 'path';
 const __dirname = path.resolve();
+
+import { Server } from 'socket.io';
 
 app.get('/', (req, res) => {
   res.send('Hello from mediasoup app!');
@@ -23,5 +26,65 @@ app.use('/sfu', express.static(path.join(__dirname, 'public')))
 const httpServer = http.createServer(app);
 httpServer.listen(3000, () => {
   console.log('LISTENING ON PORT 3000');
+});
+
+const io = new Server(httpServer);
+const peers = io.of('/mediasoup');
+
+let worker;
+let router;
+
+const createWorker = async () => {
+  worker = await mediasoup.createWorker({
+    rtcMinPort: 10000,
+    rtcMaxPort: 10100
+  });
+  console.log('worker pid', worker.pid);
+
+  worker.on('died', error => {
+    console.error('mediasoup worker has died');
+    setTimeout(() => process.exit(1), 2000); // exit in 2 seconds
+  });
+  return worker;
+}
+
+worker = createWorker();
+
+const mediaCodecs = [
+  {
+    kind: 'audio',
+    mimeType: 'audio/opus',
+    clockRate: 48000,
+    channels: 2
+  },
+  {
+    kind: 'video',
+    mimeType: 'video/VP8',
+    clockRate: 90000,
+    parameters: {
+      'x-google-start-bitrate': 1000
+    }
+  }
+];
+
+peers.on('connection', async socket => {
+  console.log(socket.id);
+  socket.emit('connection-success', {
+    socketId: socket.id
+  });
+
+  socket.on('getRtpCapabilities', (callback) => {
+    const rtpCapabilities = router.rtpCapabilities;
+    console.log('rtp capabilities', rtpCapabilities);
+    callback({ rtpCapabilities });
+  })
+
+  socket.on('disconnect', () => {
+    // do some cleanup
+    console.log('peer disconnected');
+  })
+
+  router = await worker.createRouter({ mediaCodecs });
 })
+
 
--- public/index.js
+++ public/index.js
@@ -1,1 +1,76 @@
-console.log('test');
+const io = require('socket.io-client');
+const mediasoupClient = require('mediasoup-client');
+
+const socket = io('/mediasoup');
+
+socket.on('connection-success', ({ socketId }) => {
+  console.log(socketId);
+});
+
+let device;
+let rtpCapabilities;
+
+let params = {
+  // mediasoup params
+}
+const streamSuccess = async (stream) => {
+  localVideo.srcObject = stream;
+  const track = stream.getVideoTracks()[0];
+  params = {
+    track,
+    ...params
+  }
+}
+
+const getLocalStream = () => {
+  navigator.getUserMedia({
+    // audio: true,
+    video: {
+      width: {
+        min: 640, 
+        max: 1920
+      },
+      height: {
+        min: 400,
+        max: 1080
+      }
+    }
+  }, streamSuccess, error => {
+    console.log(error.message);
+  })
+}
+
+const createDevice = async () => {
+  try {
+    console.log('createDevice()');
+    device = new mediasoupClient.Device();
+    
+    await device.load({
+      routerRtpCapabilities: rtpCapabilities
+    })
+    
+    console.log('DEVICE LOADED', device.rtpCapabilities);
+  } catch (error) {
+    console.log(error);
+    if (error.name === 'UnsupportedError') {
+      console.warn('Browser not supported');
+    }
+  }
+}
+
+const getRtpCapabilities = () => {
+  console.log('getRtpCapabilities()');
+  socket.emit('getRtpCapabilities', async (data) => {
+    rtpCapabilities = data.rtpCapabilities;
+    console.log("RTP CAPABILITIES SUCCESS", rtpCapabilities);
+    await createDevice();
+  })
+}
+
+btnLocalVideo.addEventListener('click', getLocalStream)
+btnRtpCapabilities.addEventListener('click', getRtpCapabilities)
+btnDevice.addEventListener('click', createDevice)
+// btnCreateSendTransport.addEventListener('click', createSendTransport)
+// btnConnectSendTransport.addEventListener('click', connectSendTransport)
+// btnRecvSendTransport.addEventListener('click', createRecvTransport)
+// btnConnectRecvTransport.addEventListener('click', connectRecvTransport)
